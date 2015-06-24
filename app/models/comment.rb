eeba87d238691970ae10e2977efa0d299180cff1
Katie Grogan <katgrog@hotmail.com>
2015-06-24 15:10:37 +0100

    validate comments and posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :post 
+
+	validates :body, presence: true, length: {minimum: 20} 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 
+	validates :body, presence: true
+	validates :title, presence: true
+
 	def published?
 		publication.present? and url.present? and published_at.present? 
 	end
