2a4d2cec82be543a3b359270a9343e613083250a
Katie Grogan <katgrog@hotmail.com>
2015-06-24 14:33:17 +0100

    generate comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
-  resources :posts 
   # This generates the seven RESTful Routes below: 
+  resources :posts do
+  	resources :comments, only: [:new, :create, :destroy] 
+  end
 end
